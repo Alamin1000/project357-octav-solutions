663dbc6937ec213d90fcbe5f66a94853009ac481
alamin <alaminhosen44000@gmail.com>
2023-08-01 13:41:46 +0600

    p-1

--- js/main.js
+++ js/main.js
@@ -90,6 +90,24 @@
       '<i class="far fa-chevron-right"></i>',
     ],
   });
+
+  // fullPage
+  new fullpage("#fullpage", {
+    // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+    anchors: ["page1", "page2", "page3", "page4"],
+    navigationTooltips: ["fullPage", "Open", "Easy", "Touch"],
+    css3: true,
+    scrollingSpeed: 1000,
+    navigation: true,
+    slidesNavigation: true,
+    responsiveHeight: 330,
+    dragAndMove: true,
+    dragAndMoveKey:
+      "YkdZV3gyWVhKdmRISnBaMjh1WTI5dEE3X09keFpISmhaMEZ1WkUxdmRtVT0xbmw=",
+    controlArrows: false,
+    // Get your license at https://alvarotrigo.com/fullPage/pricing/
+    licenseKey: "YOUR LICENSE KEY HERE ",
+  });
 })(jQuery);
 
 $(document).ready(function () {
@@ -97,5 +115,5 @@ $(document).ready(function () {
   $(".nice-select").niceSelect();
 
   // preloader
-  $("#preloader").fadeOut(500);
+  // $("#preloader").fadeOut(500);
 });
